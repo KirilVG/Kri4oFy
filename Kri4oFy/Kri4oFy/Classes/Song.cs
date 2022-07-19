17c49c9ea4a3ead19188150f4cb9df6a951dadb9
Kiril Gradinarov <kiril.gradinarov@grafixoft.com>
2022-07-19 15:26:17 +0300

    Implemented the song class

---
+++ Kri4oFy/Kri4oFy/Classes/Song.cs
@@ -0,0 +1,35 @@
+﻿using Kri4oFy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kri4oFy.Classes
+{
+    internal class Song : ISong
+    {
+        //fields
+        private string songName;
+        private int time;
+
+        //constructors
+        public Song(string songName)
+        {
+            this.songName = songName;
+            time = 0;
+        }
+
+        //properties
+        public string SongName 
+        {
+            get { return songName; } 
+            set { songName = value; } 
+        }
+        public int Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+    }
+}
