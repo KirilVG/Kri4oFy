9a5fa99e980dc8217bc99c803f0f7f5a62ba8210
Kiril Gradinarov <kiril.gradinarov@grafixoft.com>
2022-07-19 16:17:07 +0300

    Added the Playlist class and Interface

---
+++ Kri4oFy/Kri4oFy/Classes/PlayList.cs
@@ -0,0 +1,27 @@
+﻿using Kri4oFy.Interfaces;
+using Kri4oFy.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kri4oFy.Classes
+{
+    internal class PlayList:SongCollection ,IPlayList
+    {
+        //constructors
+        public PlayList(string PlayListName)
+            : base(PlayListName,SongCollectionTypeEnum.PlayList)
+        {
+
+        }
+
+        new public string GetFileString
+        {
+            get { return $"//<playlists><{base.CollectionName}>(songs: [{string.Join(", ",base.Songs.Select(x=>x.SongName).ToArray())}])</playlists>"; }
+        }
+
+
+    }
+}
