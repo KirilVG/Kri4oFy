f5bdd3ab6ce125535777c38d3c42d6ff251f717f
Kiril Gradinarov <kiril.gradinarov@grafixoft.com>
2022-07-19 14:16:30 +0300

    Implemented the User Class and Interface

---
+++ Kri4oFy/Kri4oFy/Classes/User.cs
@@ -0,0 +1,58 @@
+﻿using Kri4oFy.Constants;
+using Kri4oFy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kri4oFy.Classes
+{
+    internal class User : IUser
+    {
+        //fields
+        private string username;
+        private string password;
+        private UserTypeEnum type;
+
+        //constructors
+        public User(string username, string password, UserTypeEnum type)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Type = type;
+        }
+
+        //Properties
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public UserTypeEnum Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        //methods
+        public bool CheckLogInInfo(string userame, string password)
+        {
+            if (this.Username == userame && this.Password == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Kri4oFy/Kri4oFy/Interfaces/IUser.cs
@@ -0,0 +1,20 @@
+﻿using Kri4oFy.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kri4oFy.Interfaces
+{
+    internal interface IUser
+    {
+        string Username { get; set; }
+
+        string Password { get; set; }
+
+        UserTypeEnum Type { get; set; }
+
+        bool CheckLogInInfo(string userame, string password);
+    }
+}
