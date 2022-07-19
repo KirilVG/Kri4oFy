40da9a2e2078b4a4542d6ec624209338103040eb
Kiril Gradinarov <kiril.gradinarov@grafixoft.com>
2022-07-19 15:14:37 +0300

    Implemented the song collection

---
+++ Kri4oFy/Kri4oFy/Interfaces/ISongCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kri4oFy.Constants;
+
+namespace Kri4oFy.Interfaces
+{
+    internal interface ISongCollection
+    {
+        string CollectionName { get; set; }
+
+        SongCollectionTypeEnum Type { get; set; }
+
+        int Length { get; }
+
+        List<ISong> Songs { get; }
+
+        string GetFileString { get; }
+    }
+}
