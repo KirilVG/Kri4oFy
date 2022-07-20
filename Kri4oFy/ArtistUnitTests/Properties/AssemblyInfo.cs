7bbcb5a6d64b35ff1e37c640a3c565d6c31d6a23
Kiril Gradinarov <kiril.gradinarov@grafixoft.com>
2022-07-20 10:30:31 +0300

    Created the IArtistInterface and inishiated a test class

---
+++ Kri4oFy/ArtistUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArtistUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArtistUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f871f7a9-992e-4e5b-8b7a-ac1316cb51a8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Kri4oFy/Kri4oFy/Interfaces/IArtist.cs
@@ -0,0 +1,32 @@
+﻿using Kri4oFy.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kri4oFy.Interfaces
+{
+    internal interface IArtist:IUser
+    {
+        string FullName { get; set; }
+
+        DateTime DateOfBirth { get; set; }
+
+        List<GenreEnum> Genres { get; }
+
+        List<IAlbum> Albums { get; }
+
+        void PrintAlbums();
+
+        void PrintAlbumContent(string albumName);
+
+        void AddAlbum(IAlbum album);
+
+        IAlbum RemoveAlbum(string albumName);
+
+        void AddSongToAlbum(string albumName,ISong song);
+
+        ISong RemoveSongFromAlbum(string albumName,string songName);
+    }
+}
